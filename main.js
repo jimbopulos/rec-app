10bd8f338d558c6b135b1b8e06a262ebd36e132c
James Galantino <james_galantino@aol.com>
2021-01-28 14:37:22 -0500

    Added AJAX request for TheAudioDB discography API

---
+++ main.js
@@ -0,0 +1,15 @@
+// API for TheAudioDB
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://theaudiodb.p.rapidapi.com/discography.php?s=coldplay",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "164d50a740msh07c5f9842e61e21p12b55ajsn9f08d461916f",
+		"x-rapidapi-host": "theaudiodb.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
