7f494935c26f2f199cc6c407251944dbe0afd556
Carolina R <carolinarizk@gmail.com>
2021-01-30 11:08:54 -0500

    added an each function to iterate through all the different categories, select a random category, and redirect to the third screen

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
   // Unsplash API
   //Access Key = b1W5pXpGOuFqfBJJ14knI39Pa1u0UR9AJbqaZUcFJsA
   //Secret Key = 2X-XRfSskhsvflUpztL9LzRn4YjHQta3q3T9aOUeI-w
@@ -21,11 +20,28 @@ $(document).ready(function () {
   // loop through the category buttons and adding on-click events to them.
   $(".categoryBtn").each(attachCatBtnEvents);
   function attachCatBtnEvents(index, element) {
-    var id = $(element).attr("id");
+    let id = $(element).attr("id");
     $(`#${id}`).on("click", function () {
       window.location.href = `recs.html?id=${id}`;
       // searchUnsplash(id);
     });
   }
+  //make the random button make a random selection of category that will open in another page.
+  //choose a random category
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
+  $("#random").on("click", function () {
+    let allIds = [];
+    //get all the categories -iterate
+    $(".categoryBtn").each(function (index, element) {
+      let id = $(element).attr("id");
+      allIds.push(id);
+    });
+    console.log(allIds);
+    let randomIndex = getRandomNumber(0, allIds.length - 1);
+    //redirect to third screen
+    window.location.href = `recs.html?id=${allIds[randomIndex]}`;
+  });
 });
